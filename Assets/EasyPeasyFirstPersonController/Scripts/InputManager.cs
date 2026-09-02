39848aae0dab84e85624c27b82d7a76ce8470033
cameron Johnson <CameronJohnson12244@gmail.com>
2026-09-02 09:48:57 -0500

    Added Habor and Water

--- Assets/EasyPeasyFirstPersonController/Scripts/InputManager.cs
+++ Assets/EasyPeasyFirstPersonController/Scripts/InputManager.cs
@@ -14,10 +14,10 @@ namespace EasyPeasyFirstPersonController
 #if ENABLE_INPUT_SYSTEM
                 if (Keyboard.current == null) return Vector2.zero;
                 Vector2 input = Vector2.zero;
-                if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) input.y += 1;
-                if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) input.y -= 1;
-                if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) input.x -= 1;
-                if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) input.x += 1;
+                if (Keyboard.current.aKey.isPressed || Keyboard.current.upArrowKey.isPressed) input.y += 1;
+                if (Keyboard.current.dKey.isPressed || Keyboard.current.downArrowKey.isPressed) input.y -= 1;
+                if (Keyboard.current.wKey.isPressed || Keyboard.current.leftArrowKey.isPressed) input.x -= 1;
+                if (Keyboard.current.sKey.isPressed || Keyboard.current.rightArrowKey.isPressed) input.x += 1;
                 return input.normalized;
 #else
                 return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
---
+++ Assets/Scripts/CommandPattern.cs
@@ -0,0 +1,33 @@
+// using UnityEngine;
+// [DisallowMultipleComponent]
+// public class CommandPattern : MonoBehaviour
+// {
+//     public float moveDistance = 1.0f;
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
+// public interface ICommand
+// {
+//     void Execute();
+//     void Undo();
+// }
+
+// public class MoveCommend : ICommand{
+//     private readonly Action executeAction;
+//     public void Execute(){
+
+//     }
+//     public void Undo()
+//     {
+
+//     }
+// }
